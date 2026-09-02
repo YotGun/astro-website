467fc9ccb2338c750bc9be48a1fbe96300e8d3be
YotGun <reganvrn@gmail.com>
2026-09-02 05:10:41 -0600

    Start

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = 'Regan Ng';
+export const SITE_DESCRIPTION = 'Regan Ng';
+export const APP_URL = 'https://app.regan.ng';
