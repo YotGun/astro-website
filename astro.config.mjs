33352aea1968f7d41bef45b57409c3038bf39b09
YotGun <reganvrn@gmail.com>
2026-09-04 15:26:38 -0600

    Reduce worker calls and added 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://regan.ng',
 	integrations: [sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
 });
